d855d548ddb0b426f623e342d727aa2ea060c4cb
adamahmadkhan <fa19-bcs-108@cuiatd.edu.pk>
2022-01-01 21:17:47 +0500

    light gray added

--- MainDashboard.Designer.cs
+++ MainDashboard.Designer.cs
@@ -307,6 +307,7 @@ namespace Modern_Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(820, 500);
             this.Controls.Add(this.panel1);
